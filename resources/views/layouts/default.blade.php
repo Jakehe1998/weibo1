692207d5937765da90ca218c26ea61ca231f5b45
vagrant <vagrant@homestead>
2023-02-25 08:21:23 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title','weibo1 App') - Jake个人联系项目</title>
+  <title>@yield('title','weibo1 App') - Jake个人练习项目</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @yield('content')
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo1 App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item" ><a class="nav-link" href="/login">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
